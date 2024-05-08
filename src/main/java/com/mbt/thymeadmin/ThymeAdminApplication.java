5e1dd7f1f8952a85d6cfa4ebe9ab363b9afa6b52
qwy <qwy307@126.com>
2024-05-08 15:24:20 +0800

    master

---
+++ src/main/java/com/mbt/thymeadmin/ThymeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.mbt.thymeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbt/thymeadmin/ThymeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbt.thymeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
